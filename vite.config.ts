7e58dd799a9b8946e3f4c1a42e75736da8d76020
ryanch0 <ryancho@Sungwoos-MacBook-Pro.local>
2025-09-25 18:06:52 +0900

    feat: absolute path setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
+        plugins: [['babel-plugin-react-compiler']]
+      }
     }),
+    tsconfigPaths()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
